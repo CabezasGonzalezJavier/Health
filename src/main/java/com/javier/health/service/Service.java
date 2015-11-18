18c23c674a730c6fdddf033d9954f868c34e0445
Cabezas <javiergonzalezcabezas@gmail.com>
2015-11-18 18:48:00 +0000

    first commit

---
+++ src/main/java/com/javier/health/service/Service.java
@@ -0,0 +1,11 @@
+package com.javier.health.service;
+
+/**
+ * Created by javiergonzalezcabezas on 18/11/15.
+ */
+public interface Service {
+    public void get();
+    public void post();
+    public void put();
+    public void delete();
+}
---
+++ src/main/java/com/javier/health/service/UserService.java
@@ -0,0 +1,9 @@
+package com.javier.health.service;
+
+/**
+ * Created by javiergonzalezcabezas on 18/11/15.
+ */
+public class UserService {
+    void getUser(UserHandler userResponseHandler, String string);
+
+}
